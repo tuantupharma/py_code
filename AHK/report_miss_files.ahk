5788e30b71a61a81776cee3a82efb31e6bb52d43
Hoang Tuan Tu <hoangtuantu@outlook.com>
2023-10-14 20:20:00 +0700

    update

---
+++ AHK/report_miss_files.ahk
@@ -0,0 +1,16 @@
+﻿SetTitleMatchMode, 2 ; Allow flexible window title matching
+#IfWinActive, Blender
+
+; Send the keyboard shortcuts to trigger the operation
+Send, ^O ; Press Ctrl+O to open the file menu
+Sleep 1000 ; Wait for the menu to open (adjust the delay as needed)
+
+Send, e ; Press 'e' to open the External Data menu
+Sleep 500 ; Wait for the submenu to open (adjust the delay as needed)
+
+Send, r ; Press 'r' to run the Report Missing Files operation
+Sleep 1000 ; Wait for the operation to complete (adjust the delay as needed)
+
+; You can add more code here to read the report or perform other tasks
+
+#IfWinActive ; Reset the window title matching mode
---
+++ auto_restart_render.py
@@ -0,0 +1,45 @@
+import os
+import time
+import subprocess
+import glob
+
+blender_path = "C:\\Blender\\blender_3_6\\blender.exe"
+blend_file = "F:\\job\\sneaker.blend"
+render_folder = "F:\\job\\zrender"
+start_frame = 1
+end_frame = 120
+
+# Check for already rendered frames and skip them
+existing_frames = [int(os.path.splitext(os.path.basename(png))[0]) for png in glob.glob(os.path.join(render_folder, '*.png')]
+for frame in existing_frames:
+    if frame >= start_frame and frame <= end_frame:
+        start_frame = frame + 1
+
+# Start the initial Blender render
+blender_command = [blender_path, "-b", blend_file, "-s", str(start_frame), "-e", str(end_frame), "-a"]
+blender_process = subprocess.Popen(blender_command)
+
+while True:
+    time.sleep(5)  # Adjust the polling interval as needed
+
+    if blender_process.poll() is not None:
+        # Blender render process has finished
+        # Step 1: Get the name of the latest PNG file in the folder
+        latest_png = max(glob.glob(os.path.join(render_folder, '*.png')), key=os.path.getctime)
+
+        # Step 2: Kill the Blender process
+        blender_process.kill()
+
+        # Step 3: Start Blender and render from the next frame
+        frame_number = int(os.path.splitext(os.path.basename(latest_png))[0]) + 1
+        if frame_number > end_frame:
+            # All frames have been rendered
+            print("All frames rendered. Exiting.")
+            break
+        blender_command = [blender_path, "-b", blend_file, "-s", str(frame_number), "-e", str(end_frame), "-o", os.path.join(render_folder, "image_####"), "-a"]
+        blender_process = subprocess.Popen(blender_command)
+
+    if not blender_process.poll() is None:
+        # Blender process ended abruptly (for some reason)
+        print("Blender process terminated unexpectedly. Exiting.")
+        break
